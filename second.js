b70c45011ee1b088103f1999bfa183cf905279cf
moknez <deskauto23@gmail.com>
2021-02-15 19:09:16 +0100

    'methods'

--- second.js
+++ second.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course'
+            courseGoal: 'Finish the course',
+            vueLink: 'https://vue.js.org'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue';
+            } else {
+                return 'Maseter vue';
+            }
+        }
     }
 });
 
